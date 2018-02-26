8fbbb97631890b486ebfcbd1f05fc8285505899c
BB20101997 <bb-github@t-online.de>
2018-02-26 21:27:22 +0100

    fixed SubstTest sometimes running till Stack Overflow or Indefinitely

--- src/Substitution.hs
+++ src/Substitution.hs
@@ -13,7 +13,7 @@ module Substitution where
     apply::Subst -> Term -> Term
     apply (Subst [(index, term)]) orig@(Var vIndex) | index == vIndex  = term
                                                     | otherwise        = orig
-    apply (Subst subst) orig@(Var _)   = foldl (\var term -> apply (Subst [term]) var) orig subst
+    apply (Subst subst)           orig@(Var _)   = foldl (\var term -> apply (Subst [term]) var) orig subst
     apply subst (Comb s term) = Comb s (map (apply subst) term)
 
     {-
@@ -22,11 +22,11 @@ module Substitution where
     -}
     insertSubst::Subst->(VarIndex, Term)->Subst
     insertSubst (Subst [])                      a                                     = Subst [a]
-    insertSubst (Subst (head@(index, term):xs)) ins@(aIndex, aTerm) | index == aIndex = (Subst  (head:xs))
-                                                                    | otherwise       = let
-                                                                                            (Subst tail) = (insertSubst (Subst xs) ins)
-                                                                                        in
-                                                                                            (Subst  (head:tail))
+    insertSubst (Subst (head@(index, _):xs)) ins@(aIndex, _) | index == aIndex = (Subst  (head:xs))
+                                                             | otherwise       = let
+                                                                                    (Subst tail) = (insertSubst (Subst xs) ins)
+                                                                                 in
+                                                                                    (Subst  (head:tail))
 
     compose::Subst->Subst->Subst
     compose (Subst []) a          = a
@@ -42,5 +42,3 @@ module Substitution where
                                         ++ (substTupToString head)
                                         ++ [ x |tupel<-tail,x <- ","++ (substTupToString tupel) ]
                                         ++ "}"
-
-
--- test/SubstTest.hs
+++ test/SubstTest.hs
@@ -1,33 +1,35 @@
 {-# LANGUAGE TemplateHaskell #-}
 module SubstTest where
+    import Debug.Trace
     import Type
     import Pretty
     import ToBeNamed
     import Substitution
     import Test.QuickCheck
 
-    instance (Arbitrary Subst) where
-        arbitrary = do
-                        tupelList <- arbitrary
-                        return (Subst tupelList)
-
     instance (Show Subst) where
         show = pretty
 
-    instance (Arbitrary Term) where
+    instance (Arbitrary Subst) where
         arbitrary = do
-                        a <- arbitrary
-                        if a then
-                                do
-                                    vIndex <- arbitrary
+                        size <- choose (0,5)
+                        tupelVector <- vectorOf size arbitrary
+                        return (Subst tupelVector)
+
+    instance (Arbitrary Term) where
+        arbitrary = frequency [(75,arbitraryVar),(25,arbitraryTerm)]
+            where
+                arbitraryVar::Gen Term
+                arbitraryVar = do
+                                    (NonNegative vIndex) <- arbitrary
                                     return (Var vIndex)
-                            else
-                                do
-                                    tList <- arbitrary
-                                    tName <- arbitrary
+                arbitraryTerm::Gen Term
+                arbitraryTerm = do
+                                    size <- elements [0 .. 5]
+                                    tList <- vectorOf size arbitrary
+                                    nameLength <- elements [1..5]
+                                    tName <- vectorOf nameLength (choose ('a','z'))
                                     return (Comb tName tList)
-
-
 
     prop_substTest::Subst -> Subst -> Term -> Bool
     prop_substTest s1 s2 t = (apply (compose s1 s2) t) == (apply s2 (apply s1 t))
