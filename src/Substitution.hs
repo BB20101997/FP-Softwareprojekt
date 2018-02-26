7e3f704ce68c2a801bad16b8f88cff980132888b
BB20101997 <bb-github@t-online.de>
2018-02-26 21:42:27 +0100

    fixed formating

--- src/Substitution.hs
+++ src/Substitution.hs
@@ -14,14 +14,14 @@ module Substitution where
     apply (Subst [(index, term)]) orig@(Var vIndex) | index == vIndex  = term
                                                     | otherwise        = orig
     apply (Subst subst)           orig@(Var _)   = foldl (\var term -> apply (Subst [term]) var) orig subst
-    apply subst (Comb s term) = Comb s (map (apply subst) term)
+    apply subst                   (Comb s term)  = Comb s (map (apply subst) term)
 
     {-
             Inserts a Substitution Tuple into a Substitution Tuple Set
                 if no Substitution existst fo the given Variable Index
     -}
     insertSubst::Subst->(VarIndex, Term)->Subst
-    insertSubst (Subst [])                      a                                     = Subst [a]
+    insertSubst (Subst [])                      a                              = Subst [a]
     insertSubst (Subst (head@(index, _):xs)) ins@(aIndex, _) | index == aIndex = (Subst  (head:xs))
                                                              | otherwise       = let
                                                                                     (Subst tail) = (insertSubst (Subst xs) ins)
