0c1ff8a55b45a96b60ccfe3477757cf6a9d0e26a
BB20101997 <bb-github@t-online.de>
2018-02-28 20:07:42 +0100

    implemented not and \+

--- src/SLD.hs
+++ src/SLD.hs
@@ -7,24 +7,24 @@ module SLD where
     import Unifikation
 
     predefinedRulesMap::[(String,BuildInRule)]
-    predefinedRulesMap = [("call",callSubstitution),("is",evalSubstitution),("not",notSubstitution),("\\+",notSubstitution)]
+    predefinedRulesMap = [("call",callSubstitution),("is",evalSubstitution),("not",notSubstitution "not"),("\\+",notSubstitution "\\+")]
 
     predefinedRules::[Rule]
     predefinedRules = [Comb x [] :- [] | (x,_)<-predefinedRulesMap]
-
 
     {-
         For a given Program and Goal produces the corresponding SLDTree based on FIRST Selection-strategy
     -}
     sld::Strategy->Prog->Goal->SLDTree
-    sld strategy program@(Prog rules) goal = SLDTree $ catMaybes [substitute rule strategy program goal | rule <- predefinedRules++rules]
+    sld _        _                   (Goal []) = Success
+    sld strategy program@(Prog rules) goal     = SLDTree $ catMaybes [substitute rule strategy program goal | rule <- predefinedRules++rules]
         where
             substitute::Rule->BuildInRule
-            substitute  _              _        _   (Goal [])                                        = Just (empty,Success)
             substitute (Comb a _ :- _) strategy prog goal | Just func <- lookup a predefinedRulesMap = func strategy prog goal
             substitute  rule           strategy prog goal                                            = baseSubstitution rule strategy prog goal
 
     baseSubstitution::Rule->BuildInRule
+    baseSubstitution _    _        _    (Goal [])               = Nothing --this should never happen
     baseSubstitution rule strategy prog goal@(Goal (term:rest)) =   let
                                                                         (pat :- cond)      = rule >< goal
                                                                         unifier            = unify term pat
@@ -33,23 +33,23 @@ module SLD where
                                                                         Just subst  ->  let
                                                                                             newGoal = subst->>(cond++rest)
                                                                                             subTree = sld strategy prog newGoal
-                                                                                        in case newGoal of
-                                                                                            Goal [] -> Just (subst,Success)
-                                                                                            _       -> Just (subst,subTree)
+                                                                                        in Just (subst, subTree)
 
     callSubstitution::BuildInRule
     callSubstitution strategy prog (Goal (term@(Comb "call" (Comb a args:restArgs)):restGoal)) = Just (Subst[],sld strategy prog (Goal (Comb a (args++restArgs):restGoal)))
-    callSubstitution _ _    _                                  = Nothing
+    callSubstitution _        _    _                                                           = Nothing
 
     evalSubstitution::BuildInRule
-    evalSubstitution _        prog (Goal [Comb "is" [Var i, term]])     |  Just (Left  a) <- eval term = Just (single i (Comb (              show a) []), Success)
-                                                                        |  Just (Right a) <- eval term = Just (single i (Comb (map toLower $ show a) []), Success)
     evalSubstitution strategy prog (Goal (Comb "is" [Var i,term]:rest)) |  Just (Left  a) <- eval term = Just (single i (Comb (              show a) []), sld strategy prog (Goal rest))
                                                                         |  Just (Right a) <- eval term = Just (single i (Comb (map toLower $ show a) []), sld strategy prog (Goal rest))
     evalSubstitution _        _    _                                                                   = Nothing
 
-    notSubstitution::BuildInRule
-    notSubstitution _ _ _= Nothing
+
+    notSubstitution::String->BuildInRule
+    notSubstitution opCode strategy prog (Goal (Comb op goal:rest)) | op==opCode =  case strategy $ sld strategy prog (Goal goal) of
+                                                                                        [] -> Just (empty,sld strategy prog (Goal rest))
+                                                                                        _  -> Nothing
+    notSubstitution _      _        _    _                                       =            Nothing
 
     {-
         given a Rule and a Goal will replace all Variables in then rule that are also in then Goal by new ones
