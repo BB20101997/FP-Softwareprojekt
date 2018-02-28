56aa6deb56df8a219a364db9a42c2892087bc196
BB20101997 <bb-github@t-online.de>
2018-02-28 01:11:42 +0100

    goals will now be evaluated

--- src/REPL.hs
+++ src/REPL.hs
@@ -2,16 +2,20 @@ module REPL where
     --Read Eval Print Loop
     import Type
     import Parser
+    import Pretty
     import System.IO
     import Strategy
+    import Substitution
+    import SLD
 
     type State = (Strategy,Prog)
     type Action = State -> String -> IO ()
 
-    {- Todo 1. File interpreter
-            2. Goal parser
-            3. Antwortsubstitution
+    {-
+        Todo 3. Antwortsubstitution
     -}
+
+    --TODO decide if we should set a default else handle undefined strategy where necessary
     initState::State
     initState = (undefined,Prog [])
 
@@ -21,17 +25,52 @@ module REPL where
                             hFlush stdout --flushing to prevent user staring at an empty Screen
                             getLine >>= \input -> interpretPrompt state input
 
+    fileReadingResult :: State -> Either String Prog -> IO State
     fileReadingResult state                  (Left error) = putStrLn ("Couldn't read file, the following error occurred: "++error) >> return state
     fileReadingResult (strategy, oldProgram) (Right prog) = putStrLn "File read "                                                  >> return (strategy, prog)
 
     interpretPrompt :: Action
     interpretPrompt state@(strategy, program) input
+                                    | ""                       == input  = readPrompt state -- ignore empty input and ask again
                                     | ":help"                  == input  = printHelp state input
                                     | ":quit"                  == input  = exit      state input
                                     | ":info"                  == input  = printInfo state input
                                     | ":set " ++ drop 5 input  == input  = setSearch state (drop 5 input)
-                                    | ":load " ++ drop 6 input == input  = loadFile  state (drop 6 input)-- >>= \file -> readPromptWithFile file
-                                    | otherwise                          = putStrLn "Error" >> readPrompt state--Goal parser spot
+                                    | ":load " ++ drop 6 input == input  = loadFile  state (drop 6 input)
+                                    | otherwise                          = parseGoalAndEvalGoal state input
+
+    --TODO if we decide to not set a default strategy this needs to be handled here
+    parseGoalAndEvalGoal::Action
+    parseGoalAndEvalGoal state@(strategy,program) input = case parseWithVars input of
+                                                          Left error -> putStrLn error >> readPrompt state
+                                                          Right (goal,vars) -> let
+                                                                                    sldTree   = sld program goal
+                                                                                    solutions = strategy sldTree
+                                                                               in do
+                                                                                    --print sldTree
+                                                                                    --print $ pretty sldTree
+                                                                                    --print $ prettyWithVars vars sldTree
+                                                                                    outputSolutions vars solutions
+                                                                                    readPrompt state
+
+    outputSolutions::[(VarIndex,String)]->[Subst]->IO()
+    outputSolutions _ [] = putStrLn "No further Solutions!"
+    outputSolutions vars (head:rest) = do
+                                        putStr $ prettyWithVars vars head
+                                        promptFurtherSolutions  vars rest
+
+    promptFurtherSolutions::[(VarIndex,String)]->[Subst]->IO()
+    promptFurtherSolutions vars rest = do
+                                        x <- getChar
+                                        putStrLn ""
+                                        case x of
+                                            ',' -> outputSolutions vars rest    --next result
+                                            '.' -> return ()                    --don't print further results
+                                            _   -> do                           --invalid response, retry
+                                                    putStr "Invalid Command '"
+                                                    putChar x
+                                                    putStr "', valid are ',' and '.'!"
+                                                    promptFurtherSolutions vars rest
 
     setSearch :: Action
     setSearch (oldStrategy, program) newStrategy
@@ -39,18 +78,17 @@ module REPL where
                                         | newStrategy == "bfs" = putStrLn "Strategy set to breadth-first"  >> readPrompt (bfs, program)
                                         | otherwise            = putStrLn "Error strategy stays unchanged" >> readPrompt (oldStrategy, program)
 
-    fileReadingResult :: State -> Either String Prog -> IO State
     loadFile :: Action
     loadFile state filePath = do
                                 putStrLn ("Loading file "++filePath)
-                                parseResult <- parseFile filePath::IO (Either String Prog)
+                                parseResult <- parseFile filePath
                                 newState <- fileReadingResult state parseResult
                                 readPrompt newState
 
     exit::Action
     exit _ _ = putStrLn "Goodbye"
 
-    printInfo::Action
+    printInfo::Action --TODO advanced pretty print would need to have the variable names as part of the state
     printInfo state _ = putStrLn "No aviable predicates, please load file"
 
     printHelp::Action
