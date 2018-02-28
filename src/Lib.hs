4ed519a047b3c577a1cb35dcfb178bdeebcfecaf
BB20101997 <bb-github@t-online.de>
2018-02-28 15:26:58 +0100

    removed unused and maybeMap, also it exists already as Functor.fmap

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,23 +3,16 @@ module Lib where
 
     instance (Eq Term) where
         (==) (Var i) (Var i2) = i == i2
-        (==) (Comb s r) (Comb s2 r2) | s==s2 = foldl (&&) True (map (uncurry (==)) (zip r r2))
+        (==) (Comb s r) (Comb s2 r2) | s==s2 = all (uncurry (==)) (zip r r2)
         (==) _ _ = False
 
     isIn::VarIndex->Term->Bool
     isIn a (Var b) = a==b
-    isIn a (Comb b tb) = foldl (||) False (map (isIn a) tb)
+    isIn a (Comb b tb) = any (isIn a) tb
 
     varsInUse::Term->[Int]
     varsInUse (Var v)        = [v]
-    varsInUse (Comb _ terms) = [ x |term<-terms, x<-(varsInUse term)]
-
-    {-
-        A function that maps the content of a Just and preserves Nothing
-    -}
-    maybeMap::(a->b)->(Maybe a)->(Maybe b)
-    maybeMap _ Nothing = Nothing
-    maybeMap f (Just a) = Just (f a)
+    varsInUse (Comb _ terms) = [ x |term<-terms, x<-varsInUse term]
 
     {-If order is implemented for chars and list shouldn't nub
      and merge work with List of List as they work with order Types? -}
