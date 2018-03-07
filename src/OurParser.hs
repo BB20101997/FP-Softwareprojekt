845cca2f1a0a48bace1250fb9239f277edfab88d
BB20101997 <bennet-blessmann@t-online.de>
2018-03-07 14:52:11 +0100

    fixed a spelling error and documented OurParser

--- src/OurParser.hs
+++ src/OurParser.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-
+{-|
+    A modified version of the provided Parser Module
+    capable of parsing Tuple and ';'
+-}
 module OurParser
   ( Parse(..), parseFile
   ) where
@@ -11,82 +14,83 @@ import Text.Parsec hiding (parse)
 
 import Type
 
--- Type class for parsing
+-- |Type class for parsing
 class Parse a where
   parse :: String -> Either String a
   parseWithVars :: String -> Either String (a, [(VarIndex, String)])
 
   parse = fmap fst . parseWithVars
 
--- Try to parse a goal
+-- |Try to parse a goal
 instance Parse Goal where
   parseWithVars = adjustErrorMessage . simpleParse (withVars goal)
     where adjustErrorMessage (Left e) = Left ("Parse error (goal" ++ drop 19 e)
           adjustErrorMessage r        = r
 
--- Try to parse a program
+-- |Try to parse a program
 instance Parse Prog where
   parseWithVars = simpleParse (withVars prog)
 
--- Try to parse a file
+-- |Try to parse a file
 parseFile :: Parse a => FilePath -> IO (Either String a)
 parseFile fn =
   let f = reverse . dropWhile (== ' ')
   in catch (readFile (f (f fn)) >>= return . parse)
        (\ (_ :: SomeException) -> return (Left "Could not read file."))
 
--- INTERNAL
+-- |INTERNAL
 
--- Parser type
+-- |Parser type
 type Parser a = Parsec String [(String, VarIndex)] a
 
--- Apply a parser to a string
+-- |Apply a parser to a string
 simpleParse :: Parser a -> String -> Either String a
 simpleParse p =
   either (Left . ("Parse error " ++) . show) Right . runParser p [] ""
 
--- Modify a parser to return also the variable mapping
+-- |Modify a parser to return also the variable mapping
 withVars :: Parser a -> Parser (a, [(VarIndex, String)])
 withVars = flip (<*>) (map (\(x, y) -> (y, x)) <$> getState) . ((,) <$>)
 
--- Parse a goal
+-- |Parse a goal
 goal :: Parser Goal
-goal = Goal <$> (whitespaces *> ((:[]) <$> simTerm) <* symbol "." <* eof)
+goal = Goal <$> (whitespaces *> ((:[]) <$> semTerm) <* symbol "." <* eof)
 
--- Parse a program
+-- |Parse a program
 prog :: Parser Prog
 prog = Prog <$> (whitespaces *> many rule <* eof)
 
--- Parse a rule
+-- |Parse a rule
 rule :: Parser Rule
 rule = (:-) <$> lhs <*> rhs
 
--- Parse the left hand side of a rule
+-- |Parse the left hand side of a rule
 lhs :: Parser Term
 lhs = try (parens lhs) <|> comb
 
--- Parse the right hand side of a rule
+-- |Parse the right hand side of a rule
 rhs :: Parser [Term]
-rhs = symbol "." *> pure [] <|> symbol ":-" *> ((:[])  <$> simTerm) <* symbol "."
+rhs = symbol "." *> pure [] <|> symbol ":-" *> ((:[])  <$> semTerm) <* symbol "."
 
-
-simTerm :: Parser Term
-simTerm = do
+-- |Parse semicolon separated comTerms
+semTerm :: Parser Term
+semTerm = do
     args@(_:_) <- comTerm `sepBy` symbol ";"
     whitespaces
     pure $ foldr1 (\x xs -> Comb ";" [x,xs]) args
 
--- Parse a Tuple
+-- |Parse comma separated baseTerms
 comTerm :: Parser Term
 comTerm = do
     args@(_:_) <- baseTerm `sepBy` symbol ","
     whitespaces
     pure $ foldr1 (\x xs -> Comb  "," [x, xs]) args
 
+-- |Parse a list, variable, combinator or parenthesized semTerm
 baseTerm :: Parser Term
-baseTerm = list <|> var <|> comb <|> parens simTerm
+baseTerm = list <|> var <|> comb <|> parens semTerm
 
--- Parse a variable term
+-- |Parse a variable term
 var :: Parser Term
 var = Var <$> do
   name <- varName
@@ -99,12 +103,12 @@ var = Var <$> do
       Just idx -> pure idx
       Nothing  -> newIdx
 
--- Parse a variable name
+-- |Parse a variable name
 varName :: Parser String
 varName = ((:) <$> upper <*> many (letter <|> digit <|> char '_') <|>
   (:) <$> char '_' <*> many (letter <|> digit <|> char '_')) <* whitespaces
 
--- Parse a list
+-- |Parse a list
 list :: Parser Term
 list = symbol "[" *> whitespaces *> do
   let nil = Comb "[]" []
@@ -113,7 +117,7 @@ list = symbol "[" *> whitespaces *> do
         baseTerm <* symbol "]") <|>
     foldr cons nil <$> commaSep baseTerm <* symbol "]"
 
--- Parse a combination term
+-- |Parse a combination term
 comb :: Parser Term
 comb = do
     f <- atom
@@ -121,12 +125,12 @@ comb = do
     whitespaces
     pure (Comb f args)
 
--- Parse an atom
+-- |Parse an atom
 atom :: Parser String
 atom = (:) <$> lower <*> many (letter <|> digit <|> char '_') <|>
   number <|> (many1 (oneOf "+-*/<=>'\\:.?@#$&^~") <?> "symbol")
 
--- Parse a number
+-- |Parse a number
 number :: Parser String
 number = try ((:) <$> char '-' <*> (try float <|> int)) <|> try float <|> int
   where float = (++) <$> int <*>
@@ -134,23 +138,23 @@ number = try ((:) <$> char '-' <*> (try float <|> int)) <|> try float <|> int
         int   = trim <$> many1 digit
         trim  = show . (read :: String -> Integer)
 
--- Parse a symbol
+-- |Parse a symbol
 symbol :: String -> Parser ()
 symbol s = string s *> whitespaces
 
--- Parse a comment
+-- |Parse a comment
 comment :: Parser ()
 comment = () <$ (char '%' *> many (noneOf "\n") *> char '\n') <?> "comment"
 
--- Parse whitespaces or a comment
+-- |Parse whitespaces or a comment
 whitespaces :: Parser ()
 whitespaces =  skipMany (() <$ space <|> comment)
 
--- Parse a list separated by commas
+-- |Parse a list separated by commas
 commaSep :: Parser a -> Parser [a]
 commaSep p = p `sepBy` symbol ","
 
--- Parse something enclosed in parentheses
+-- |Parse something enclosed in parentheses
 parens :: Parser a -> Parser a
 parens = between (symbol "(") (symbol ")")
 
