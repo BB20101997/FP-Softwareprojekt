409dc9ad75164653871cd784cd78e63cc3b30254
Sloner42 <sloner@web.de>
2018-03-08 10:29:28 +0100

    Main now displays the default values in the welcome message (reverted from commit 1a9f42f2ddf111d31582c1e3b1b3634d7c7b14d7)

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,4 @@ module Main where
     main = do
             putStrLn "Welcome to Simple Prolog!"
             putStrLn "Type \":help\" for help."
-            putStrLn "Default search is depth-first."
-            putStrLn ("Default Parser is OurParser that"++
-             " adds parsing for tuple and ';'.")
             REPL.readPrompt REPL.initState
