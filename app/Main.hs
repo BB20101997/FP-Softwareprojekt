bd4f6fd86a8fda0de61a84718ff6f3ddffae539c
Sloner42 <sloner@web.de>
2018-03-08 10:33:02 +0100

    Main now displays dedault values in welcome message

--- app/Main.hs
+++ app/Main.hs
@@ -5,5 +5,8 @@ module Main where
     main :: IO ()
     main = do
             putStrLn "Welcome to Simple Prolog!"
+            putStrLn "Default search is depth-first."
+            putStrLn ("Default parser is OurParser"++
+                      " that adds parsing for Tupel and ';'.")
             putStrLn "Type \":help\" for help."
             REPL.readPrompt REPL.initState
